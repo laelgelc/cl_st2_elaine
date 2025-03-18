ed8f49dd5c650a6be7de2b27c93d59302672dc5c
Rogério Yamada <eyamrog@gmail.com>
2025-03-17 23:39:28 -0300

    20250317-2339

---
+++ ph0/ph01/cl_st2_elaine_error_ph01.sh
@@ -0,0 +1,84 @@
+DELAY=2
+#python getwebpage.py 000096 https://www.ted.com/talks/pete_alcorn_s_vision_of_a_better_world/transcript?language=en
+#sleep $DELAY
+#python getwebpage.py 000154 https://www.ted.com/talks/dan_ariely_on_our_buggy_moral_code/transcript?language=en
+#sleep $DELAY
+#python getwebpage.py 000166 https://www.ted.com/talks/kamal_meattle_on_how_to_grow_your_own_fresh_air/transcript?language=en
+#sleep $DELAY
+#python getwebpage.py 000252 https://www.ted.com/talks/caleb_chung_plays_with_pleo/transcript?language=en
+#sleep $DELAY
+#python getwebpage.py 000290 https://www.ted.com/talks/chris_abani_muses_on_humanity/transcript?language=en
+#sleep $DELAY
+#python getwebpage.py 000425 https://www.ted.com/talks/vusi_mahlasela_sings_thula_mama/transcript?language=en
+#sleep $DELAY
+#python getwebpage.py 000500 https://www.ted.com/talks/william_mcdonough_on_cradle_to_cradle_design/transcript?language=en
+#sleep $DELAY
+#python getwebpage.py 000540 https://www.ted.com/talks/barry_schwartz_on_the_paradox_of_choice/transcript?language=en
+#sleep $DELAY
+python getwebpage.py 000877 https://www.ted.com/talks/anthony_mccarten_a_not_so_scientific_experiment_on_laughter/transcript?language=en
+sleep $DELAY
+#python getwebpage.py 000990 https://www.ted.com/talks/gaspard_koenig_do_we_really_own_our_bodies/transcript?language=en
+#sleep $DELAY
+#python getwebpage.py 001067 https://www.ted.com/talks/graham_shaw_why_people_believe_they_can_t_draw/transcript?language=en
+#sleep $DELAY
+python getwebpage.py 001348 https://www.ted.com/talks/cynthia_erivo_natural_woman_and_johnny_and_donna/transcript?language=en
+sleep $DELAY
+#python getwebpage.py 001355 https://www.ted.com/talks/gus_casely_hayford_the_powerful_stories_that_shaped_africa/transcript?language=en
+#sleep $DELAY
+#python getwebpage.py 001767 https://www.ted.com/talks/rebecca_mackinnon_we_can_fight_terror_without_sacrificing_our_rights/transcript?language=en
+#sleep $DELAY
+python getwebpage.py 001890 https://www.ted.com/talks/caleb_harper_this_computer_will_grow_your_food_in_the_future/transcript?language=en
+sleep $DELAY
+python getwebpage.py 001974 https://www.ted.com/talks/lucianne_walkowicz_let_s_not_use_mars_as_a_backup_planet/transcript?language=en
+sleep $DELAY
+#python getwebpage.py 002137 https://www.ted.com/talks/alix_generous_how_i_learned_to_communicate_my_inner_life_with_asperger_s/transcript?language=en
+#sleep $DELAY
+#python getwebpage.py 002176 https://www.ted.com/talks/cosmin_mihaiu_physical_therapy_is_boring_play_a_game_instead/transcript?language=en
+#sleep $DELAY
+#python getwebpage.py 002317 https://www.ted.com/talks/alison_sander_megatrends_the_art_and_science_of_trend_tracking/transcript?language=en
+#sleep $DELAY
+#python getwebpage.py 002350 https://www.ted.com/talks/yusuf_nurbhai_what_if_we_traded_employees_like_football_players/transcript?language=en
+#sleep $DELAY
+python getwebpage.py 002410 https://www.ted.com/talks/meaghan_ramsey_why_thinking_you_re_ugly_is_bad_for_you/transcript?language=en
+sleep $DELAY
+#python getwebpage.py 002413 https://www.ted.com/talks/moshe_safdie_how_to_reinvent_the_apartment_building/transcript?language=en
+#sleep $DELAY
+#python getwebpage.py 002490 https://www.ted.com/talks/sajan_saini_how_do_self_driving_cars_see/transcript?language=en
+#sleep $DELAY
+#python getwebpage.py 002526 https://www.ted.com/talks/shai_reshef_a_tuition_free_college_degree/transcript?language=en
+#sleep $DELAY
+python getwebpage.py 002699 https://www.ted.com/talks/renee_hlozek_the_death_of_the_universe/transcript?language=en
+sleep $DELAY
+#python getwebpage.py 002710 https://www.ted.com/talks/toni_griffin_a_new_vision_for_rebuilding_detroit/transcript?language=en
+#sleep $DELAY
+python getwebpage.py 002770 https://www.ted.com/talks/lucianne_walkowicz_light_waves_visible_and_invisible/transcript?language=en
+sleep $DELAY
+#python getwebpage.py 002792 https://www.ted.com/talks/eleanor_longden_the_voices_in_my_head/transcript?language=en
+#sleep $DELAY
+python getwebpage.py 002967 https://www.ted.com/talks/asha_de_vos_why_are_blue_whales_so_enormous/transcript?language=en
+sleep $DELAY
+#python getwebpage.py 002975 https://www.ted.com/talks/dan_pallotta_the_way_we_think_about_charity_is_dead_wrong/transcript?language=en
+#sleep $DELAY
+#python getwebpage.py 002976 https://www.ted.com/talks/danny_hillis_the_internet_could_crash_we_need_a_plan_b/transcript?language=en
+#sleep $DELAY
+#python getwebpage.py 002980 https://www.ted.com/talks/eric_whitacre_virtual_choir_live/transcript?language=en
+#sleep $DELAY
+#python getwebpage.py 002981 https://www.ted.com/talks/francis_collins_we_need_better_drugs_now/transcript?language=en
+#sleep $DELAY
+#python getwebpage.py 003020 https://www.ted.com/talks/jonathan_haidt_how_common_threats_can_make_common_political_ground/transcript?language=en
+#sleep $DELAY
+#python getwebpage.py 003221 https://www.ted.com/talks/carl_schoonover_how_to_look_inside_the_brain/transcript?language=en
+#sleep $DELAY
+python getwebpage.py 003287 https://www.ted.com/talks/ayah_bdeir_building_blocks_that_blink_beep_and_teach/transcript?language=en
+sleep $DELAY
+python getwebpage.py 003339 https://www.ted.com/talks/lucianne_walkowicz_look_up_for_a_change/transcript?language=en
+sleep $DELAY
+#python getwebpage.py 003424 https://www.ted.com/talks/srdja_popovic_how_to_topple_a_dictator/transcript?language=en
+#sleep $DELAY
+python getwebpage.py 003472 https://www.ted.com/talks/lucianne_walkowicz_finding_planets_around_other_stars/transcript?language=en
+sleep $DELAY
+#python getwebpage.py 003563 https://www.ted.com/talks/jr_s_ted_prize_wish_use_art_to_turn_the_world_inside_out/transcript?language=en
+#sleep $DELAY
+#python getwebpage.py 003565 https://www.ted.com/talks/mick_ebeling_the_invention_that_unlocked_a_locked_in_artist/transcript?language=en
+#sleep $DELAY
+#python getwebpage.py 003901 https://www.ted.com/talks/ryan_lobo_through_the_lens_of_compassion/transcript?language=en
---
+++ ph1/cl_st2_elaine_error_ph1.sh
@@ -0,0 +1,66 @@
+DELAY=2
+python getwebpage.py 000009 https://www.ted.com/talks/charles_duhigg_the_science_behind_dramatically_better_conversations_mar_2025/transcript?language=en
+sleep $DELAY
+python getwebpage.py 000509 https://www.ted.com/talks/daniel_nickrent_why_does_this_flower_smell_like_a_dead_body/transcript?language=en
+sleep $DELAY
+python getwebpage.py 001135 https://www.ted.com/talks/vaitea_cowan_how_green_hydrogen_could_end_the_fossil_fuel_era/transcript?language=en
+sleep $DELAY
+python getwebpage.py 001154 https://www.ted.com/talks/jeanette_winterson_is_humanity_smart_enough_to_survive_itself/transcript?language=en
+sleep $DELAY
+python getwebpage.py 001195 https://www.ted.com/talks/liesbeth_demuyser_what_causes_yeast_infections_and_how_do_you_get_rid_of_them/transcript?language=en
+sleep $DELAY
+python getwebpage.py 001377 https://www.ted.com/talks/wajahat_ali_3_lessons_on_hope_in_challenging_times/transcript?language=en
+sleep $DELAY
+python getwebpage.py 001399 https://www.ted.com/talks/ozawa_bineshi_albert_climate_action_needs_new_frontline_leadership/transcript?language=en
+sleep $DELAY
+python getwebpage.py 001488 https://www.ted.com/talks/peachy_liv_how_to_protect_your_mental_well_being_online_from_a_gen_zer/transcript?language=en
+sleep $DELAY
+python getwebpage.py 001490 https://www.ted.com/talks/valerie_june_astral_plane/transcript?language=en
+sleep $DELAY
+python getwebpage.py 001785 https://www.ted.com/talks/alex_gendler_demolition_disease_and_death_building_the_panama_canal/transcript?language=en
+sleep $DELAY
+python getwebpage.py 001841 https://www.ted.com/talks/chase_ladue_and_bruce_a_schulte_the_incredible_bendable_twistable_expandable_elephant_trunk/transcript?language=en
+sleep $DELAY
+python getwebpage.py 002310 https://www.ted.com/talks/marah_j_hardt_are_we_interrupting_the_kinky_sex_lives_of_fish/transcript?language=en
+sleep $DELAY
+python getwebpage.py 002424 https://www.ted.com/talks/patrick_lencioni_are_you_an_ideal_team_player/transcript?language=en
+sleep $DELAY
+python getwebpage.py 002502 https://www.ted.com/talks/paul_franklin_the_art_and_science_of_special_effects/transcript?language=en
+sleep $DELAY
+python getwebpage.py 002934 https://www.ted.com/talks/laura_caccia_the_avant_garde_frontier_of_code_poetry/transcript?language=en
+sleep $DELAY
+python getwebpage.py 002935 https://www.ted.com/talks/ian_barnes_what_archaeology_and_dna_can_teach_us_about_prehistoric_migration/transcript?language=en
+sleep $DELAY
+python getwebpage.py 003097 https://www.ted.com/talks/adeline_cuvelier_and_toril_rokseth_how_does_the_nobel_peace_prize_work/transcript?language=en
+sleep $DELAY
+python getwebpage.py 003156 https://www.ted.com/talks/tien_nguyen_how_do_pregnancy_tests_work/transcript?language=en
+sleep $DELAY
+python getwebpage.py 003188 https://www.ted.com/talks/mikael_fogelstrom_graphene_the_impressive_2d_material_full_of_potential/transcript?language=en
+sleep $DELAY
+python getwebpage.py 003268 https://www.ted.com/talks/kevin_p_boyd_why_do_we_have_to_wear_sunscreen/transcript?language=en
+sleep $DELAY
+python getwebpage.py 003432 https://www.ted.com/talks/paul_nicklen_animal_tales_from_icy_wonderlands/transcript?language=en
+sleep $DELAY
+python getwebpage.py 003469 https://www.ted.com/talks/zainab_salbi_women_wartime_and_the_dream_of_peace/transcript?language=en
+sleep $DELAY
+python getwebpage.py 003471 https://www.ted.com/talks/eric_berlow_simplifying_complexity/transcript?language=en
+sleep $DELAY
+python getwebpage.py 003473 https://www.ted.com/talks/andrew_bird_a_one_man_orchestra_of_the_imagination/transcript?language=en
+sleep $DELAY
+python getwebpage.py 003483 https://www.ted.com/talks/tim_jackson_an_economic_reality_check/transcript?language=en
+sleep $DELAY
+python getwebpage.py 003529 https://www.ted.com/talks/enric_sala_glimpses_of_a_pristine_ocean/transcript?language=en
+sleep $DELAY
+python getwebpage.py 003530 https://www.ted.com/talks/thomas_dolby_love_is_a_loaded_pistol/transcript?language=en
+sleep $DELAY
+python getwebpage.py 003531 https://www.ted.com/talks/jeremy_jackson_how_we_wrecked_the_ocean/transcript?language=en
+sleep $DELAY
+python getwebpage.py 003535 https://www.ted.com/talks/james_randi_homeopathy_quackery_and_fraud/transcript?language=en
+sleep $DELAY
+python getwebpage.py 003540 https://www.ted.com/talks/elizabeth_pisani_sex_drugs_and_hiv_let_s_get_rational/transcript?language=en
+sleep $DELAY
+python getwebpage.py 003893 https://www.ted.com/talks/pamelia_kurstin_the_untouchable_music_of_the_theremin/transcript?language=en
+sleep $DELAY
+python getwebpage.py 003997 https://www.ted.com/talks/alex_steffen_the_route_to_a_sustainable_future/transcript?language=en
+sleep $DELAY
+python getwebpage.py 004068 https://www.ted.com/talks/jeff_han_the_radical_promise_of_the_multi_touch_interface/transcript?language=en
