d6fa0f490407fe5b64f28d85204a204eda068a23
Rogério Yamada <eyamrog@gmail.com>
2026-08-25 16:42:14 -0300

    gitall_20260825-1642

--- cl_st2_ph4_elaine/cl_st2_ph4_elaine.sh
+++ cl_st2_ph4_elaine/cl_st2_ph4_elaine.sh
@@ -40,48 +40,12 @@ python keylemmas.py \
 # ------------------------------------------------------------
 # 3. Select a stratified keyword set
 #
-# Selects up to 250 keywords per decade, with a maximum of 1200
+# Selects up to 250 keywords per year, with a maximum of 1200
 # keywords before final de-duplication. The final keyword list is
 # used to construct binary keyword columns for SAS.
 # ------------------------------------------------------------
 
-# Run 1 - Deprecated
-
-python select_kws_stratified.py \
-    --per-decade 250 \
-    --max-total 1200
-# Output: corpus/09_kw_selected/keywords.txt
-
-"
-=== Decade Keyword Quotas ===
-1950   → 250 keywords max
-1960   → 250 keywords max
-1970   → 250 keywords max
-1980   → 250 keywords max
-1990   → 250 keywords max
-2000   → 250 keywords max
-2010   → 250 keywords max
-2020   → 250 keywords max
-=============================
-
-1950   → selected 187/250 from 187 available POSKW lemmas
-1960   → selected 49/250 from 49 available POSKW lemmas
-1970   → selected 8/250 from 8 available POSKW lemmas
-1980   → selected 6/250 from 6 available POSKW lemmas
-1990   → selected 6/250 from 6 available POSKW lemmas
-2000   → selected 7/250 from 7 available POSKW lemmas
-2010   → selected 10/250 from 10 available POSKW lemmas
-2020   → selected 16/250 from 16 available POSKW lemmas
-
-Total consolidated keywords before de-duplication: 289
-Unique keywords after de-duplication: 265
-Duplicates removed: 24
-
-Final unique keywords written to: corpus/09_kw_selected/keywords.txt
-Final unique keyword count: 265
-"
-
-# Run 2 - Considering lowercase alphabetic characters,
+# Run 1 - Considering lowercase alphabetic characters,
 # optionally joined by internal hyphens
 
 python select_kws_stratified.py \
